fbe7d6bc481a6fd22ed8f794398f9a5297a934b6
Maria Aranibar <saranibarp@outlook.com>
2015-02-05 00:37:06 +0000

    Added controllers, viewes and stuff

---
+++ app/controllers/body_types_controller.rb
@@ -0,0 +1,9 @@
+class BodyTypesController < ApplicationController
+  def index
+    @body_types = BodyType.order('name ASC')
+  end
+
+  def show
+    @body_type = BodyType.find(params[:id])
+  end
+end
--- app/models/body.rb
+++ app/models/body.rb
@@ -1,3 +1,7 @@
 class Body < ActiveRecord::Base
   belongs_to :body_type
+
+  def diameter_in_kms
+    relative_diameter * 12742
+  end
 end
